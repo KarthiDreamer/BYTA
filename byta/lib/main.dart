125eda97fd7c1f4e17031d6041fea7c6353a41aa
Karthi <karthikeyan.jayhind@gmail.com>
2023-05-19 16:59:32 +0530

    added splash screen

--- byta/lib/main.dart
+++ byta/lib/main.dart
@@ -1,8 +1,16 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,29 +21,46 @@ class MyApp extends StatelessWidget {
       title: 'BYTA',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Home Page'),
+      home: const MyHomePage(title: 'BYTA'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key,required this.title});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState(){
+    super.initState();
+    // void super.initState();
+    FlutterNativeSplash.remove();
+
+  }
+
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: const Center(
-        child: Text(
-          'Hello, world!',
-          style: TextStyle(fontSize: 24),
-        ),
+        child: Text('Welcome to BYTA'),
       ),
     );
+
   }
 }
+
+// TODO: Dashboard with 2 options child wallet , parent wallet and pay for me button
+// bottom navigation bar too same thing apply
+
